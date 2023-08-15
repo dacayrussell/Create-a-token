c394983e4c3039d416f53d30393f5132c1ddf68d
dacayrussell <142073440+dacayrussell@users.noreply.github.com>
2023-08-16 02:44:50 +0800

    Update token.sol

--- token.sol
+++ token.sol
@@ -1,31 +1,27 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity 0.8.18;
 
-contract myToken {
-    string public name;
-    string public symbol;
-    uint256 public totalSupply;
+contract MyToken {
 
-    mapping(address => uint256) public balanceOf;
+    // public variables here
+    string public TokenName = "Russell";
+    string public TokenAbbrv = "Russ";
+    uint public TotalSupply = 0;
+    
+    // mapping variable here
+    mapping(address => uint) public balances;
 
-    constructor(string memory _name, string memory _symbol, uint256 _initialSupply) {
-        name = _name;
-        symbol = _symbol;
-        totalSupply = _initialSupply;
-        balanceOf[msg.sender] = _initialSupply;
+    // mint function
+    function mint (address _address, uint _value) public{
+       TotalSupply += _value;
+       balances[_address] += _value;
     }
-
-    function mint(address _receiver, uint256 _amount) public {
-        require(_amount > 0, "Amount must be greater than 0");
-        totalSupply += _amount;
-        balanceOf[_receiver] += _amount;
-    }
-
-    function burn(address _owner, uint256 _amount) public {
-        require(_amount > 0, "Amount must be greater than 0");
-        require(balanceOf[_owner] >= _amount, "Insufficient balance");
-        
-        totalSupply -= _amount;
-        balanceOf[_owner] -= _amount;
-    }
+    // burn function
+   function burn (address _address, uint _value) public{
+      if (balances[_address] >= _value) {
+      TotalSupply -= _value;
+      balances[_address] -= _value;
+      }
+   }
 }
+
